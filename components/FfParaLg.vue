f4e6dc4a1e522774bd7836539716739a5f2ecd2c
Philipp Wagner <mail@philipp-wagner.com>
2023-03-28 21:12:42 -0700

    Large paragraph: Remove TODO for phone font size
    The style guide now describes the correct font size for the phone view,
    remove the TODO.
    
    Fixes #5

--- components/FfParaLg.vue
+++ components/FfParaLg.vue
@@ -5,13 +5,7 @@ Use for shorter blocks of text
 
 Style guide references:
 - Web components/Typography/Paragraphs/large paragraph
-
-TODO:
-- Large paragraphs appear next to headings (such as h2 and h3). Headings scale
-  their font size, but the large paragraph doesn't -- this makes a h3 followed
-  by a large paragraph look out of proportion on phone/tablet views. We
-  currently are choosing a smaller font from the 1.25x-scale for the phone view.
-  Update the style guide if that's correct.
+- Web components/Typography/Paragraphs/large paragraph phone
 -->
 <template>
   <p class="text-phone-20 tablet:text-24 font-bold leading-tighter mb-[30px] tablet:mb-[36px] max-w-prose">
