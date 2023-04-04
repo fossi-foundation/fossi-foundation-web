c4108d339c638a60706a1ecae7b4e5a9efcb354f
Philipp Wagner <mail@philipp-wagner.com>
2023-04-03 22:47:45 -0700

    Setup code highlighting
    Enable code highlighting for Python and bash code. Provide suitable Vue
    components, and configure Nuxt Content to highlight code blocks in
    markdown.
    
    Deviate from the style guide in that we are using a light colored
    background for code samples, instead of the specified warm-black one, to
    work around the inability to specify different syntax highlighting
    themes for inline and block code.

---
+++ components/content/ProseCode.vue
@@ -0,0 +1,46 @@
+<script lang="ts">
+import { defineComponent } from '#imports'
+
+export default defineComponent({
+  props: {
+    code: {
+      type: String,
+      default: ''
+    },
+    language: {
+      type: String,
+      default: null
+    },
+    filename: {
+      type: String,
+      default: null
+    },
+    highlights: {
+      type: Array as () => number[],
+      default: () => []
+    },
+    meta: {
+      type: String,
+      default: null
+    }
+  }
+})
+</script>
+
+<template>
+  <slot />
+</template>
+
+<style>
+/* We cannot use FfCodeBlock here, but keep the styling in sync with it. */
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+pre code .line {
+  @apply font-mono;
+  @apply p-20;
+  @apply leading-normal;
+  @apply bg-pastel-grey;
+}
+</style>
