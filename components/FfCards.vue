592dacf0f97c4d4877ca8a88e806bb2ee71b9c32
Philipp Wagner <mail@philipp-wagner.com>
2023-09-08 12:27:51 +0200

    Card layout: increase whitespace
    Increase gap and vertical padding on card layout.

--- components/FfCards.vue
+++ components/FfCards.vue
@@ -6,7 +6,7 @@ A container for cards.
 Style guide reference: Web components/Cards/Card layout
 -->
 <template>
-  <div class="grid grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-3 gap-16">
+  <div class="grid grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-3 gap-24 tablet:gap-32 py-24 tablet:py-48 desktop:py-64">
     <slot></slot>
   </div>
 </template>
