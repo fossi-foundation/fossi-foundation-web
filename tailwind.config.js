0f5010a1496e4aee8036485bc41bf3aeaa12651d
Philipp Wagner <mail@philipp-wagner.com>
2023-09-08 19:32:49 +0200

    Cleanup: Remove unused import

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,6 @@
  ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
  */
 
-import defaultTheme from 'tailwindcss/defaultTheme'
-
 export default {
   theme: {
     extend: {
