3eeb46f07acc5a134ae41ee51bb1321fa42bcc25
Philipp Wagner <mail@philipp-wagner.com>
2023-09-09 16:29:43 +0200

    Only show subnavigation if necessary
    Only show the subnavigation if pages beside the index page exist in the
    corresponding "category".
    
    Remove outdated TODO comments along the way.

--- components/FfNavSub.vue
+++ components/FfNavSub.vue
@@ -4,12 +4,9 @@ Sub-navigation
 Used on section pages that contain child pages.
 
 Style guide reference: Web components/Navigation/Sub-navigation
-
-TODO:
-- The logic to query the subpages is broken.
 -->
 <template>
-  <nav class="py-20 font-black text-ultraviolet">
+  <nav class="py-20 font-black text-ultraviolet" v-if="hasSubpages">
     <ContentNavigation v-slot="{ navigation }" :query="subpagesQuery">
       <ul class="flex flex-col tablet:flex-row tablet:space-x-16 px-0 mx-0">
         <li class="block" v-for="link of navigation[0].children">
@@ -23,6 +20,13 @@ TODO:
 </template>
 
 <script setup lang="ts">
-/* extract the basename (first sub-path and query content from there) */
-const subpagesQuery = queryContent(useRoute().path.split("/")[1])
+// Extract the first path segment and use it as "category".
+const category = useRoute().path.split("/")[1]
+const subpagesQuery = queryContent(category)
+
+// A category has at least one page: the index page. Show the subnavigation only
+// if there are more pages than the index page.
+// TODO: Switch to .count() once we update to Nuxt Content 2.8
+// const hasSubpages = (await queryContent(category).count()) > 1
+const hasSubpages = ((await queryContent(category).find()).length ?? 0) > 1
 </script>
