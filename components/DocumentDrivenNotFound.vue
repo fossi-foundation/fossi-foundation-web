d0709926f0098974501a9e44e24b5b07a6866594
Philipp Wagner <mail@philipp-wagner.com>
2023-09-09 23:29:54 +0200

    Update 404 page

--- components/DocumentDrivenNotFound.vue
+++ components/DocumentDrivenNotFound.vue
@@ -2,10 +2,22 @@
 404 (Not Found) error page.
 
 This component is used by Nuxt Content in document-driven mode.
-
-TODO:
-* Add styling.
 -->
 <template>
-  <h1>Page not found</h1>
+  <FfHeaderText title="Page not found" header="We couldn't find the content you were looking for under this address."/>
+  <FfContainer>
+    <FfContentLong>
+      <FfPara>
+        Maybe you can find the content you're looking for using the navigation above.
+        Or try to locate it on the old website at
+        <FfLink to="https://www-archive.fossi-foundation.org">www-archive.fossi-foundation.org</FfLink>.
+      </FfPara>
+      <FfPara>
+        Please
+        <FfLink to="https://github.com/fossi-foundation/fossi-foundation-web/issues">reach out on GitHub</FfLink>
+        if you think the content you're looking for, or a redirect, should be
+        added to this website.
+      </FfPara>
+    </FfContentLong>
+  </FfContainer>
 </template>
