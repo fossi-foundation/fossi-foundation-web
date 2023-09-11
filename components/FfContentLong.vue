de8a39743a51f3a66767f431c436a628ec87cafe
Philipp Wagner <mail@philipp-wagner.com>
2023-09-11 21:05:32 +0200

    Long form content: Increase vertical whitespace

--- components/FfContentLong.vue
+++ components/FfContentLong.vue
@@ -6,7 +6,7 @@ Use to present news post text and page body text
 Style guide reference: Web components/Content blocks/Longform content
 -->
 <template>
-  <div class="max-w-[640px] mx-auto">
+  <div class="max-w-[640px] mx-auto py-24 tablet:py-48 desktop:py-64">
     <slot></slot>
   </div>
 </template>
