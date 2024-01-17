41a7062d2a623463e1728abfd3d77b60db291393
Philipp Wagner <mail@philipp-wagner.com>
2024-01-17 22:27:08 +0100

    New component: FfSupportingContent
    A new component, presenting content with a gray background.

---
+++ components/FfSupportingContent.vue
@@ -0,0 +1,12 @@
+<!--
+Supporting content block
+
+A full-width block with gray background for supporting content.
+-->
+<template>
+  <div class="bg-pastel-grey">
+    <FfContainer class="py-24 tablet:py-64">
+      <ContentSlot :use="$slots.default"/>
+    </FfContainer>
+  </div>
+</template>
