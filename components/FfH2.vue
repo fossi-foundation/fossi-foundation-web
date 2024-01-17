1a4e5d940315d1388afe627ad1623acb3ed77f91
Philipp Wagner <mail@philipp-wagner.com>
2024-01-17 22:21:32 +0100

    H2/H3: Use padding instead of margin
    Keep the background in place when doing spacing around headers.

--- components/FfH2.vue
+++ components/FfH2.vue
@@ -6,7 +6,7 @@ Style guide references:
 - Web components/Typography/Smaller headings for phone view/h2 phone
 -->
 <template>
-  <h2 class="font-black text-phone-25 tablet:text-36 tracking-tight leading-tighter text-ultraviolet mt-[34px] mb-[22px] tablet:mb-[18px] tablet:mt-[30px] max-w-prose" :id="id">
+  <h2 class="font-black text-phone-25 tablet:text-36 tracking-tight leading-tighter text-ultraviolet pt-[34px] pb-[22px] tablet:pb-[18px] tablet:pt-[30px] max-w-prose" :id="id">
     <ContentSlot :use="$slots.default" unwrap="p" />
   </h2>
 </template>
--- components/FfH3.vue
+++ components/FfH3.vue
@@ -6,7 +6,7 @@ Style guide references:
 - Web components/Typography/Smaller headings for phone view/h3 phone
 -->
 <template>
-  <h3 class="font-black text-phone-20 tablet:text-24 tracking-tight leading-tighter text-ultraviolet mt-[22px] mb-10 tablet:mt-24 tablet:mb-12 max-w-prose" :id="id">
+  <h3 class="font-black text-phone-20 tablet:text-24 tracking-tight leading-tighter text-ultraviolet pt-[22px] pb-10 tablet:pt-24 tablet:pb-12 max-w-prose" :id="id">
     <ContentSlot :use="$slots.default" unwrap="p" />
   </h3>
 </template>
