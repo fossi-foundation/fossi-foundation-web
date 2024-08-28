5d55a46ce79e74c4e7dc7f947633394f75c09dbf
Philipp Wagner <mail@philipp-wagner.com>
2024-08-28 20:45:16 +0200

    Add Chalmers as major sponsor

--- components/ff/event/Sponsor.vue
+++ components/ff/event/Sponsor.vue
@@ -1,12 +1,12 @@
 <template>
-    <NuxtLink v-if="link" :to="link" class="self-center" :class="levelClass">
+    <NuxtLink v-if="link" :to="link" class="p-10 self-center" :class="levelClass">
         <NuxtImg v-bind="$attrs" />
     </NuxtLink>
-    <NuxtImg v-else class="self-center" :class="levelClass" />
+    <NuxtImg v-else class="p-10 self-center" :class="levelClass" />
 </template>
 
 <script setup lang="ts">
 const props = defineProps(['link', 'level'])
 
-const levelClass = props.level == 'headline' ? 'col-span-3' : '';
+const levelClass = (props.level == 'headline' || props.level == 'major') ? 'col-span-3' : '';
 </script>
