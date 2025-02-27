12f83761100f10fa0bdad8f4acb491bc37e65fd1
Anvita Prasad <cs23b1059.iiitdm.ac.in>
2025-02-27 16:50:19 +0530

    Enhanced CTA button with hover effect

--- components/FfBtnCta.vue
+++ components/FfBtnCta.vue
@@ -1,26 +1,24 @@
 <!--
 Call-to-action button
-
 TODO:
 - Font scaling is implemented, but not specified in the style guide.
 -->
-
 <template>
-  <NuxtLink class="inline-block w-full tablet:w-auto text-center font-black text-phone-20 tablet:text-24 px-20 py-10 rounded-tr-xl shadow-mid" :class="{ 'bg-ultraviolet': !inverted, 'bg-white': inverted, 'text-white': !inverted, 'text-ultraviolet': inverted }" :to="linkTo" :title="linkTitle" :target="linkTarget">
+  <NuxtLink
+    class="inline-block w-full tablet:w-auto text-center font-black text-phone-20 tablet:text-24 px-20 py-10 rounded-tr-xl shadow-mid transition-transform duration-300 hover:scale-105"
+    :class="{ 'bg-ultraviolet': !inverted, 'bg-white': inverted, 'text-white': !inverted, 'text-ultraviolet': inverted }"
+    :to="linkTo" :title="linkTitle" :target="linkTarget">
     <ContentSlot :use="$slots.default" unwrap="p" />
   </NuxtLink>
 </template>
-
 <script setup lang="ts">
 export interface Props {
   // Invert the color scheme.
   inverted?: boolean
-
   linkTo: string,
   linkTitle?: string
   linkTarget?: string
 }
-
 withDefaults(defineProps<Props>(), {
   inverted: false
 })
