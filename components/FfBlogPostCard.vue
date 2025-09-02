b92252cb557b9d65acb6edf90772daafa0f3c490
Philipp Wagner <mail@philipp-wagner.com>
2025-09-02 07:16:17 +0000

    WIP on blog posts

--- components/FfBlogPostCard.vue
+++ components/FfBlogPostCard.vue
@@ -2,15 +2,15 @@
 Signpost Card summarizing a single post
 -->
 <template>
-  <FfCardSignpost :key="post._path" :headline="post.title" :linkTo="post._path" :img="post.coverImage">
-    <ContentRendererMarkdown :value="post" :excerpt="true" />
+  <FfCardSignpost :key="post.path" :headline="post.title" :linkTo="post.path" :img="post.coverImage">
+    <ContentRenderer :value="post" :excerpt="true" />
   </FfCardSignpost>
 </template>
 
 <script setup lang="ts">
 export interface Post {
   title: string
-  _path: string
+  path: string
   coverImage?: string
 }
 
