bc09933f5718fcee8d767e33224af5c35b2493f4
Leo Moser <leomoser99@gmail.com>
2026-02-27 10:09:51 +0100

    Add new components for LibreLane
    Signed-off-by: Leo Moser <leomoser99@gmail.com>

---
+++ components/FfCardTestimonial.vue
@@ -0,0 +1,48 @@
+<!--
+Information card
+
+Use to present chunks of short information such as a member profile or project
+summary
+
+The card image has to passed as HTML in a named slot due to Vite/Nuxt
+limitations, which effectively prevent passing an image name as prop.
+See https://github.com/nuxt/nuxt/issues/14766 for a discussion.
+
+Style guide reference: Web components/Cards/Information card
+-->
+<template>
+  <NuxtLink :to="linkTo" :title="linkTitle" :target="linkTarget" class="hover:shadow-low bg-white">
+  <div class="max-w-[350px] mx-auto">
+    <div class="relative mx-auto h-[155px] w-[350px]">
+      <img :src="img" class="h-[155px] w-[350px] object-contain" />
+    </div>
+    <div class="text-center">
+      <FfH3 class="text-ultraviolet font-black">{{ headline }}</FfH3>
+      <FfParaExcerpt v-if="subheading" class="mt-10 text-ultraviolet">
+        {{ subheading }}
+      </FfParaExcerpt>
+      <FfParaExcerpt class="text-warm-black mt-10">
+        <slot></slot>
+      </FfParaExcerpt>
+    </div>
+  </div>
+    </NuxtLink>
+</template>
+
+
+<script setup lang="ts">
+export interface Props {
+  // Image
+  img?: string
+
+  headline: string
+  subheading?: string
+
+  // Link-related properties. No link is set if linkTo is undefined.
+  linkTo?: string
+  linkTitle?: string
+  linkTarget?: string
+}
+
+defineProps<Props>()
+</script>
---
+++ components/FfLibreLaneNews.vue
@@ -0,0 +1,31 @@
+<!--
+Homepage
+-->
+<template>
+
+  <!--
+  News
+  -->
+  <div class="bg-pastel-grey">
+    <FfContainer class="py-32 bg-pastel-grey">
+      <FfH3 class="text-warm-black text-center mx-auto mb-32">
+        Latest LibreLane News
+      </FfH3>
+
+      <FfCards>
+        <FfBlogPostCard v-for="post in blogPosts" :post="post" />
+      </FfCards>
+    </FfContainer>
+  </div>
+
+</template>
+
+<script setup>
+// Get the three most recent blog posts
+const blogPosts = await queryContent('/librelane/blog')
+.sort({date: -1 }) // show latest articles first
+.where({_partial: false })
+.where({ _id: { $ne: 'content:librelane:blog:index.md' } }) // Filter out the blog overview page.
+.limit(3)
+.find()
+</script>
