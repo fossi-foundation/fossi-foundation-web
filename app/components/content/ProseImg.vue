9fb898fce8d645d3e4e7e9bb29a93019f8561a63
Philipp Wagner <mail@philipp-wagner.com>
2026-05-11 09:42:12 +0000

    Update prose components to match current upstream

--- app/components/content/ProseImg.vue
+++ app/components/content/ProseImg.vue
@@ -1,4 +1,46 @@
-<!-- components/content/ProseImg.vue -->
 <template>
-  <nuxt-img />
+  <component
+    :is="ImageComponent"
+    :src="refinedSrc"
+    :alt="props.alt"
+    :width="props.width"
+    :height="props.height"
+  />
 </template>
+
+<script setup lang="ts">
+import { withTrailingSlash, withLeadingSlash, joinURL } from 'ufo'
+import { useRuntimeConfig, computed } from '#imports'
+
+import ImageComponent from '#build/mdc-image-component.mjs'
+
+const props = defineProps({
+  src: {
+    type: String,
+    default: '',
+  },
+  alt: {
+    type: String,
+    default: '',
+  },
+  width: {
+    type: [String, Number],
+    default: undefined,
+  },
+  height: {
+    type: [String, Number],
+    default: undefined,
+  },
+})
+
+const refinedSrc = computed(() => {
+  if (props.src?.startsWith('/') && !props.src.startsWith('//')) {
+    // @ts-expect-error runtime config is not typed
+    const _base = withLeadingSlash(withTrailingSlash(useRuntimeConfig().app.baseURL))
+    if (_base !== '/' && !props.src.startsWith(_base)) {
+      return joinURL(_base, props.src)
+    }
+  }
+  return props.src
+})
+</script>
